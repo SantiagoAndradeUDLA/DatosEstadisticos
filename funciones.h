38553cd2b4a534f0a1d0f441ef550adef94362e5
SantiagoAndradeUDLA <santiago.andrade.bucheli@udla.edu.ec>
2024-11-17 23:12:11 -0500

    Create funciones.h

---
+++ funciones.h
@@ -0,0 +1,10 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+void ordenarArreglo(float numeros[], float numordenados[], 
+                    float *min, float *max, float *mediana, 
+                    int tamano);
+
+void mediaymoda(float numeros[], int tamano, float *media, float *moda);
+
+#endif
